d02cb38c0c3ace95e252fca33473e8364a57b4fc
samskull <samskull>
2025-04-07 23:03:59 +0600

    Week-2 Day-2 Problem-5 Solved

---
+++ Week-2/Day-2/MaximumMultipleSum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        cout << (n == 3 ? 3 : 2) << endl;
+    }
+    return 0;
+}
