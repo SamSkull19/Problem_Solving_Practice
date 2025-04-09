3743881dfe6a5de46393c83166f06cab147dfb20
samskull <samskull>
2025-04-09 22:27:47 +0600

    Week-2 Day-4 Problem-3 Solved

---
+++ Week-2/Day-4/BreakingSticks.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector <int> v(n);
+        int sum = 0;
+        for(auto &x : v){ 
+            cin >> x;
+            sum += (x - 1);
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
