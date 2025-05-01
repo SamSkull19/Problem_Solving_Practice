e12818a5c68de9022ea66e0ef9f287fb7d364422
sifat-samin <sifatsamin19@gmail.com>
2025-05-01 21:43:18 +0600

    Week-3 Day-7 Problem-1 Solved

---
+++ Week-3/Day-7/PermutationWarm-Up.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int x = n * n;
+        cout << (x / 4) + 1 << endl;
+    }
+    return 0;
+}
