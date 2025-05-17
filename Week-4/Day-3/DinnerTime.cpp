86ab7b2cac41267ac0e38156467e425710a9dafd
sifat-samin <sifatsamin19@gmail.com>
2025-05-17 14:18:37 +0600

    Week-4 Day-3 Problem-1 Solved

---
+++ Week-4/Day-3/DinnerTime.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, m, p, q;
+        cin >> n >> m >> p >> q;
+
+        if(n % p == 0 && (n / p) * q != m){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
