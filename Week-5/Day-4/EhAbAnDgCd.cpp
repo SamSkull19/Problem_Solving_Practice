133a01edc93131284bcadd58d66780b7d8622441
sifat-samin <sifatsamin19@gmail.com>
2025-06-03 11:22:35 +0600

    Week-5 Day-4 Problem-3 Solved

---
+++ Week-5/Day-4/EhAbAnDgCd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        ll x;
+        cin >> x;
+        cout << 1 << " " << x - 1 << endl;
+    }
+    return 0;
+}
