6a95116afbf78ea414f34aa2d0bc060d2dc50f5e
sifat-samin <sifatsamin19@gmail.com>
2025-06-13 14:29:23 +0600

    Week-6 Day-2 Problem3 Solved

---
+++ Week-6/Day-2/Palindrome.py
@@ -0,0 +1,16 @@
+n = input()
+x = n[::-1]
+
+c = 0
+for i in x:
+    if i == '0':
+        c += 1
+    else:
+        break
+
+print(x[c : ])
+
+if n == x[c:]:
+    print("YES")
+else:
+    print("NO")
