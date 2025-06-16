bd1ac0d9dd4c3c9a0a379c21ebb0e11377c3691b
sifat-samin <sifatsamin19@gmail.com>
2025-06-16 18:49:06 +0600

    Week-6 Day-2 Problem-7 Solved

---
+++ Week-6/Day-2/SmallestPair.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+while t > 0:
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    sum = float('inf')
+    for i in range(n):
+        for j in range(i + 1, n):
+            s = a[i] + a[j] + j - i
+            sum = min(sum, s)
+
+    print(sum)
+    t -= 1
