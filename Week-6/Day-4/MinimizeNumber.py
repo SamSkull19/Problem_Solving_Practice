eea1e50d05bd1e1bdb6df438d6449cb5c08f8dc8
sifat-samin <sifatsamin19@gmail.com>
2025-06-18 21:39:07 +0600

    Week-6 Day-4 Problem-3 Solved

---
+++ Week-6/Day-4/MinimizeNumber.py
@@ -0,0 +1,12 @@
+n = input()
+a = list(map(int, input().split()))
+
+cnt = 0
+while True:
+    if all(x % 2 == 0 for x in a):
+        a = [x // 2 for x in a]
+        cnt += 1
+    else:
+        break
+    
+print(cnt)
