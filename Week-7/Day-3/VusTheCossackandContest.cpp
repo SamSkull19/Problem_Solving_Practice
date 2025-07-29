2f4b5d964f22ce8b42b05a28a7d5571fc3cc7ebd
sifat-samin <sifatsamin19@gmail.com>
2025-07-29 22:04:32 +0600

    Week-7 Day-3 Problem-2 Solved

---
+++ Week-7/Day-3/VusTheCossackandContest.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, y, z;
+    cin >> x >> y >> z;
+    y = min(y, z);
+    
+    if(x <= y) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
