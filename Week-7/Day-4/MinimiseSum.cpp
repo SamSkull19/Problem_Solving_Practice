67c6733789d67b6eeafe853077d1a93ade1892dd
sifat-samin <sifatsamin19@gmail.com>
2025-08-08 20:37:13 +0600

    Week-7 Day-4 Problem-6 Solved

---
+++ Week-7/Day-4/MinimiseSum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector <int> v(n);
+        for(auto &x : v) cin >> x;
+        cout << min(2 * v[0], v[0] + v[1]) << endl;
+    }
+    return 0;
+}
