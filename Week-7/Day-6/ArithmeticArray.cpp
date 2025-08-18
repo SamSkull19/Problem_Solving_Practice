2b1a168a475f72f3460bb52d8e289e0b624c42ef
sifat-samin <sifatsamin19@gmail.com>
2025-08-18 12:33:02 +0600

    Week-7 Day-6 Problem-4 Solved

---
+++ Week-7/Day-6/ArithmeticArray.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector <int> v(n);
+        int sum = 0;
+        for(auto &x : v){ 
+            cin >> x;
+            sum += x;
+        }
+
+        if(n > sum){
+            cout << 1 << endl;
+            continue;
+        }
+        cout << sum - n << endl;
+    }
+    return 0;
+}
